99838383c05938808a0ab50e9b6488faf0c26735
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-08-17 16:32:41 -0400

    Complete publishing dance party

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -31,7 +31,3 @@ makeBlinkyDancer.prototype.step = function() {
 
   this.$node.toggle();
 };
-
-// we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-// so we must keep a copy of the old version of this function
-// var oldStep = makeDancer.prototype.step;
